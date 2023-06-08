bd90f300a20f89951d3478b59e29d5d7634b226d
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-06-08 11:29:24 +0200

    08.06.2023 axios from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Product } from "./components/Product";
-import { products } from "./data/products";
+import { IProduct } from "./models";
+import axios from "axios";
+import { AxiosError } from "axios";
 
 function App() {
+  const [products, setProducts] = useState<IProduct[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  async function fetchProducts() {
+    try {
+      setError("");
+      setLoading(true);
+      const response = await axios.get<IProduct[]>(
+        "https://fakestoreapi.com/products?limit=5"
+      );
+      setProducts(response.data);
+      setLoading(false);
+    } catch (e: unknown) {
+      const error = e as AxiosError;
+      setLoading(false);
+      setError(error.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      <Product product={products[0]}/>
-      <Product product={products[1]}/>
+      {loading && <p className="text-center">Loading...</p>}
+      {error && <p className="text-center text-red-400">{error}</p>}
+      {products.map((product) => (
+        <Product product={product} key={product.id} />
+      ))}
+
+      {/* <Product product={products[0]}/>
+      <Product product={products[1]}/> */}
     </div>
   );
 }
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { IProduct } from "../models";
 
 interface ProductProps {
@@ -6,12 +6,31 @@ interface ProductProps {
 }
 
 export function Product({ product }: ProductProps) {
+  const [details, setDetails] = useState(false);
+
+  const btnBgClassName = details ? 'bg-yellow-400' : 'bg-blue-400'
+  const btnClasses = ['py-2 px-4 border', btnBgClassName]
   return (
     <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
       <img src={product.image} className="w-1/6" alt={product.title} />
       <p>{product.title}</p>
       <p className="font-bold">{product.price}</p>
-      <button className="py-2 px-4 border bg-yellow-400">Show Details</button>
+      {/* <button className="py-2 px-4 border bg-yellow-400"
+       onClick={()=>setDetails(true)}>Show Details</button>
+      <button className="py-2 px-4 border bg-blue-400"
+       onClick={()=>setDetails(false)}>Hide Details</button> */}
+      <button
+        className={btnClasses.join(' ')}
+        onClick={() => setDetails((prev) => !prev)}
+      >
+        {details ? "Hide Details" : "Show Details"}
+      </button>
+      {details && (
+        <div>
+          <p>{product.description}</p>
+          <p>Rate: <span style={{fontWeight: 'bold'}}>{product.rating.rate}</span></p>
+        </div>
+      )}
     </div>
   );
 }
